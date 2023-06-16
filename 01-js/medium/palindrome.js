76ef5095138a784904f3f3ffe7edd6d45f0f7bff
ShrutiSharma77 <shruti.sharma.met21@itbhu.ac.in>
2023-06-17 01:46:29 +0530

    solved palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,7 +7,22 @@
 */
 
 function isPalindrome(str) {
-  return true;
+const alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+const myArray = str.toLowerCase().split('');
+const myString = myArray.filter(element => alpha.includes(element))
+
+const reverseString = [];
+for (let i = myString.length-1; i >= 0; i--) {
+    reverseString.push(myString[i]);
+}
+
+if(myString.join('')===reverseString.join('')){
+    return true;
+}
+else{
+    return false;
+}
 }
 
 module.exports = isPalindrome;
